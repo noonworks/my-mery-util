f4c888745a5a683a9629e4b627c5b3b70e2659b4
noonworks <noonworks@gmail.com>
2014-03-27 01:09:10 +0900

    Refactoring launchers.

--- Launcher.js
+++ Launcher.js
@@ -1,155 +1,76 @@
-if (typeof Noonworks === 'undefined') {
-    var Noonworks = {};
-}
-if (typeof Noonworks.Launcher === 'undefined') {
-    Noonworks.Launcher = {};
-    Noonworks.Launcher.fso = new ActiveXObject('Scripting.FileSystemObject');
-    Noonworks.Launcher.shell = new ActiveXObject('WScript.Shell');
-    
-    Noonworks.Launcher.createDirRecursive = function(path) {
-        var stack = new Array();
-        while ((! this.fso.FolderExists(path)) && (! this.fso.FileExists(path))) {
-            parent = this.fso.GetParentFolderName(path);
-            if (this.fso.FolderExists(parent)) {
-                try {
-                    this.fso.CreateFolder(path);
-                    while (stack.length > 0) {
-                        path = this.fso.BuildPath(path, stack.pop());
-                        this.fso.CreateFolder(path);
-                    }
-                } catch (e) {
-                    return false;
-                }
-                break;
-            }
-            stack.push(this.fso.GetFileName(path));
-            path = parent;
-        }
-        if (this.fso.FileExists(path)) {
-            return false;
-        }
-        return true;
-    };
-    
-    Noonworks.Launcher.openMery = function(path) {
-        Editor.OpenFile(path, meEncodingNone, meOpenAllowNewWindow);
-    };
-    Noonworks.Launcher.createOpenMeryFunc = function(path) {
-        return function() {
-            Noonworks.Launcher.openMery(path);
+#include "FileSystemObject.js"
+
+var Noonworks = Noonworks || {};
+
+Noonworks.Launcher = function(){
+    this.fso = new Noonworks.FileSystemObject();
+    this.shell = new ActiveXObject('WScript.Shell');
+};
+
+Noonworks.Launcher.prototype = {
+    makeClosure: function(func, args) {
+        return function(){
+            func.apply(this, args);
         };
-    };
+    },
     
-    Noonworks.Launcher.createWithMery = function(path) {
-        this.createDirRecursive(this.fso.GetParentFolderName(path));
+    createDirRecursive: function(path) {
+        return this.fso.createDirRecursive(path);
+    },
+    
+    openMery: function(path) {
+        Editor.OpenFile(path, meEncodingNone, meOpenAllowNewWindow);
+    },
+    
+    createWithMery: function(path) {
+        this.fso.createDirRecursive(this.fso.GetParentFolderName(path));
         this.fso.CreateTextFile(path, true);
         this.openMery(path);
-    };
-    Noonworks.Launcher.createCreateWithMeryFunc = function(path) {
-        return function() {
-            Noonworks.Launcher.createWithMery(path);
-        };
-    };
+    },
     
-    Noonworks.Launcher.openCmd = function(path) {
-        if (path == '') {
-            path = Document.Path;
-            if (path == '') {
-                path = this.shell.SpecialFolders('Desktop');
-            }
-        }
+    openCmd: function(path) {
+        path = path || Document.Path || this.shell.SpecialFolders('Desktop');
         this.shell.CurrentDirectory = path;
         this.shell.Run('cmd');
-    };
-    Noonworks.Launcher.createOpenCmdFunc = function(path) {
-        return function() {
-            Noonworks.Launcher.openCmd(path);
-        };
-    };
+    },
     
-    Noonworks.Launcher.openExplorer = function(path) {
-        if (path == '') {
-            path = Document.Path;
-            if (path == '') {
-                path = this.shell.SpecialFolders('Desktop');
-            }
-        }
-        this.shell.CurrentDirectory = path;
+    openExplorer: function(path) {
+        path = path || Document.Path || this.shell.SpecialFolders('Desktop');
         this.shell.Run('explorer.exe "' + path + '"');
-    };
-    Noonworks.Launcher.createOpenExplorerFunc = function(path) {
-        return function() {
-            Noonworks.Launcher.openExplorer(path);
-        };
-    };
+    },
     
-    Noonworks.Launcher.openUrl = function(url) {
+    openUrl: function(url) {
         if (url.indexOf('http://') < 0 && url.indexOf('https://') < 0) {
             url = 'http://' + url;
         }
         this.shell.Run(url);
-    };
-    Noonworks.Launcher.createOpenUrlFunc = function(path) {
-        return function() {
-            Noonworks.Launcher.openUrl(path);
-        };
-    };
+    },
     
-    Noonworks.Launcher.open = function(str) {
+    open: function(str) {
         this.shell.Run(str);
-    };
-    Noonworks.Launcher.createOpenFunc = function(path) {
-        return function() {
-            Noonworks.Launcher.open(path);
-        };
-    };
+    },
     
-    Noonworks.Launcher.search = function(str, next) {
-        var direction = next ? meFindNext : meFindPrevious;
-        Document.selection.Find(str, direction);
-    };
-    Noonworks.Launcher.createSearchFunc = function(str, next) {
-        return function() {
-            Noonworks.Launcher.search(str, next);
-        };
-    };
+    search: function(str, next) {
+        Document.selection.Find(str, (next ? meFindNext : meFindPrevious));
+    },
     
-    Noonworks.Launcher.searchGoogle = function(str) {
+    searchGoogle: function(str) {
         var url = 'https://www.google.co.jp/search?q=' + encodeURI(str);
-        Noonworks.Launcher.openUrl(url);
-    };
-    Noonworks.Launcher.createSearchGoogleFunc = function(str) {
-        return function() {
-            Noonworks.Launcher.searchGoogle(str);
-        };
-    };
+        this.openUrl(url);
+    },
     
-    Noonworks.Launcher.searchWikipedia = function(str) {
+    searchWikipedia: function(str) {
         var url = 'http://ja.wikipedia.org/w/index.php?search=' + encodeURI(str);
-        Noonworks.Launcher.openUrl(url);
-    };
-    Noonworks.Launcher.createSearchWikipediaFunc = function(str) {
-        return function() {
-            Noonworks.Launcher.searchWikipedia(str);
-        };
-    };
+        this.openUrl(url);
+    },
     
-    Noonworks.Launcher.openGoogleTranslateJaToEn = function(str) {
+    openGoogleTranslateJaToEn: function(str) {
         var url = 'https://translate.google.co.jp/#ja/en/' + encodeURI(str);
-        Noonworks.Launcher.openUrl(url);
-    };
-    Noonworks.Launcher.createOpenGoogleTranslateJaToEnFunc = function(str) {
-        return function() {
-            Noonworks.Launcher.openGoogleTranslateJaToEn(str);
-        };
-    };
-    Noonworks.Launcher.openGoogleTranslateEnToJa = function(str) {
+        this.openUrl(url);
+    },
+    
+    openGoogleTranslateEnToJa: function(str) {
         var url = 'https://translate.google.co.jp/#en/ja/' + encodeURI(str);
-        Noonworks.Launcher.openUrl(url);
-    };
-    Noonworks.Launcher.createOpenGoogleTranslateEnToJaFunc = function(str) {
-        return function() {
-            Noonworks.Launcher.openGoogleTranslateEnToJa(str);
-        };
-    };
-}
+        this.openUrl(url);
+    }
+};
--- NLauncher.js
+++ NLauncher.js
@@ -9,113 +9,109 @@
     var URL_MINIFY_LEN = 30;
     var PATH_MINIFY_LEN = 40;
     var WORD_MINIFY_LEN = 50;
+    var alp = /^[a-zA-Z0-9 \t　\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~]*$/;
+    
     var sel = Noonworks.Selector.getSelectionInfo();
     //alert(Noonworks.Selector.dumpSelectionInfo(sel));
+    var lnc = new Noonworks.Launcher();
     var mb = new Noonworks.MenuBuilder();
+    
     // open url
     var items = new Array(sel.url, sel.url_raw);
     for (var i = 0; i < items.length; i++) {
         if (items[i].length > 0) {
-            mb.addItem('URLを開く[' + mb.minify(items[i], URL_MINIFY_LEN) + ']',
-                Noonworks.Launcher.createOpenUrlFunc(items[i]));
+            with({ item:items[i] }) {
+                mb.addItem('URLを開く[' + mb.minify(item, URL_MINIFY_LEN) + ']',
+                    function(){ lnc.openUrl(item) });
+            } // end with scope
         }
     }
-    if (mb.count > 0) {
-        mb.need_sep = true;
-    }
-    var counts = mb.count;
+    mb.need_sep = mb.isGrown();
+    
     // open path menu
-    items = new Array(sel.path, sel.path_raw);
-    if (mb.count > 0) {
-        items = new Array();
-    }
+    items = (mb.count > 0) ? new Array() : new Array(sel.path, sel.path_raw);
     for (var i = 0; i < items.length; i++) {
         if (items[i].length == 0) {
             continue;
         }
-        var p = Noonworks.Selector.getPathInfo(items[i]);
         var submb = new Noonworks.MenuBuilder();
-        if (p.isDir) {  // Directory
+        with({ item : items[i],
+               p : Noonworks.Selector.getPathInfo(items[i]) }) {
+            // open if exists
             if (p.isExist) {
-                submb.addItem('エクスプローラで開く',
-                    Noonworks.Launcher.createOpenExplorerFunc(items[i]));
-                submb.addItem('ここでコマンドプロンプトを開く',
-                    Noonworks.Launcher.createOpenCmdFunc(items[i]));
+                if (p.isDir) {
+                    submb.addItem('エクスプローラで開く',
+                        function(){ lnc.openExplorer(item) });
+                    submb.addItem('ここでコマンドプロンプトを開く',
+                        function(){ lnc.openCmd(item) });
+                } else  {
+                    submb.addItem('Meryで開く',
+                        function(){ lnc.openMery(item) });
+                    submb.addItem('関連付けされたプログラムで開く',
+                        function(){ lnc.open(item) });
+                }
             } else {
-                submb.addItem('このフォルダを作る', (function(path){ return function() {
-                    if (Confirm('以下のフォルダを作成します。\n' + path)) {
-                        if (! Noonworks.Launcher.createDirRecursive(path)) {
-                            alert('フォルダ作成に失敗しました。\n' + path);
-                        } else {
-                            Status = 'フォルダ作成：' + path;
+            // create if not exists
+                if (p.isDir) {
+                    submb.addItem('このフォルダを作る', function(){
+                        if (Confirm('以下のフォルダを作成します。\n' + item)) {
+                            if (! lnc.createDirRecursive(item)) {
+                                alert('フォルダ作成に失敗しました。\n' + item);
+                            } else {
+                                Status = 'フォルダ作成：' + item;
+                            }
                         }
-                    }
-                };})(items[i]));
-            }
-            if (submb.count > 0) {
-                submb.need_sep = true;
-            }
-            if (p.parentExists) {
-                submb.addItem('親フォルダをエクスプローラで開く',
-                    Noonworks.Launcher.createOpenExplorerFunc(p.parent));
-                submb.addItem('親フォルダでコマンドプロンプトを開く',
-                    Noonworks.Launcher.createOpenCmdFunc(p.parent));
-            }
-        } else {    // File
-            if (p.isExist) {
-                submb.addItem('Meryで開く',
-                    Noonworks.Launcher.createOpenMeryFunc(items[i]));
-                submb.addItem('関連付けされたプログラムで開く',
-                    Noonworks.Launcher.createOpenFunc(items[i]));
-            } else {
-                submb.addItem('Meryで作成',
-                    (function(path, parent, parentExists){ return function() {
-                        if ((! parentExists) &&
-                            Confirm('親フォルダを作成して開きます。\n' + parent)) {
-                            if (! Noonworks.Launcher.createDirRecursive(parent)) {
-                                alert('フォルダ作成に失敗しました。\n' + parent);
+                    });
+                } else {
+                    submb.addItem('Meryで作成', function() {
+                        if (! p.parentExists) {
+                            if (!Confirm('親フォルダを作成して開きます。\n' + p.parent)) {
+                                return;
+                            }
+                            if (! lnc.createDirRecursive(p.parent)) {
+                                alert('フォルダ作成に失敗しました。\n' + p.parent);
                                 return;
                             } else {
-                                Status = 'フォルダ作成：' + parent;
+                                Status = 'フォルダ作成：' + p.parent;
                             }
                         }
-                        Noonworks.Launcher.createWithMery(path);
-                    };})(items[i], p.parent, p.parentExists));
+                        lnc.createWithMery(item);
+                    });
+                }
             }
-            if (submb.count > 0) {
-                submb.need_sep = true;
-            }
+            // add separator if needed
+            submb.need_sep = submb.isGrown();
+            // open parent folder if exists
             if (p.parentExists) {
                 submb.addItem('親フォルダをエクスプローラで開く',
-                    Noonworks.Launcher.createOpenExplorerFunc(p.parent));
+                    function(){ lnc.openExplorer(p.parent) });
                 submb.addItem('親フォルダでコマンドプロンプトを開く',
-                    Noonworks.Launcher.createOpenCmdFunc(p.parent));
+                    function(){ lnc.openCmd(p.parent) });
             } else {
-                submb.addItem('親フォルダを作る',
-                    (function(parent){ return function() {
-                        if (Confirm('以下のフォルダを作成します。\n' + parent)) {
-                            if (! Noonworks.Launcher.createDirRecursive(parent)) {
-                                alert('フォルダ作成に失敗しました。\n' + parent);
-                            } else {
-                                Status = 'フォルダ作成：' + parent;
-                            }
+            // create parent folder if not exists
+                submb.addItem('親フォルダを作る', function() {
+                    if (Confirm('以下のフォルダを作成します。\n' + p.parent)) {
+                        if (! lnc.createDirRecursive(p.parent)) {
+                            alert('フォルダ作成に失敗しました。\n' + p.parent);
+                        } else {
+                            Status = 'フォルダ作成：' + p.parent;
                         }
-                    };})(p.parent));
+                    }
+                });
             }
-        }
-        if (submb.count > 0) {
-            var prefix = p.isFile ? 'ファイル: ' : 'フォルダ: ';
-            mb.addItem(prefix + mb.minify(items[i], PATH_MINIFY_LEN), submb.menu);
-        }
+            // add submenu
+            if (submb.count > 0) {
+                var prefix = p.isFile ? 'ファイル: ' : 'フォルダ: ';
+                mb.addItem(prefix + mb.minify(item, PATH_MINIFY_LEN), submb.menu);
+            }
+        } // end with scope
     }
-    if (mb.count > counts) {
-        mb.need_sep = true;
-    }
-    counts = mb.count;
+    mb.need_sep = mb.isGrown();
+    
+    // keyword menu
     items = new Array(sel.sel, sel.d_quoted, sel.s_quoted,
         sel.word, sel.word_c, sel.sentence);
     var inserted = new Array();
-    var alp = /^[a-zA-Z0-9 \t　\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~]*$/;
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
         for (var j = 0; j < inserted.length; j++) {
@@ -129,25 +125,24 @@
         }
         inserted.push(item);
         var submb = new Noonworks.MenuBuilder();
-        submb.addItem('前を検索',
-            Noonworks.Launcher.createSearchFunc(item, false));
-        submb.addItem('次を検索',
-            Noonworks.Launcher.createSearchFunc(item, true));
-        submb.addSep();
-        submb.addItem('Googleで検索',
-            Noonworks.Launcher.createSearchGoogleFunc(item));
-        if (alp.test(item)) {
-            submb.addItem('Google翻訳で翻訳（英→日）',
-                Noonworks.Launcher.createOpenGoogleTranslateEnToJaFunc(item));
-        } else {
-            submb.addItem('Google翻訳で翻訳（日→英）',
-                Noonworks.Launcher.createOpenGoogleTranslateJaToEnFunc(item));
-        }
-        submb.addSep();
-        submb.addItem('Wikipediaで検索',
-            Noonworks.Launcher.createSearchWikipediaFunc(item));
+        with({ item:item }) {
+            submb.addItem('前を検索', function(){ lnc.search(item, false) });
+            submb.addItem('次を検索', function(){ lnc.search(item, true) });
+            submb.addSep();
+            submb.addItem('Googleで検索', function(){ lnc.searchGoogle(item) });
+            if (alp.test(item)) {
+                submb.addItem('Google翻訳で翻訳（英→日）',
+                    function(){ lnc.openGoogleTranslateEnToJa(item) });
+            } else {
+                submb.addItem('Google翻訳で翻訳（日→英）',
+                    function(){ lnc.openGoogleTranslateJaToEn(item) });
+            }
+            submb.addSep();
+            submb.addItem('Wikipediaで検索', function(){ lnc.searchWikipedia(item) });
+        } // end with scope
         mb.addItem(mb.minify(item, WORD_MINIFY_LEN), submb.menu);
     }
+    
     // show
     if (mb.count > 0) {
         ShowPopupMenu(mb.menu);
